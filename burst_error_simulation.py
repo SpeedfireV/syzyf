da2f2cd026edae6ac7dccc34e45228e78fc59462
Grzegorz Niespodziany <grzegorz.niespodzianek@gmail.com>
2024-04-22 01:26:18 +0200

    fixed burst error simulation

--- burst_error_simulation.py
+++ burst_error_simulation.py
@@ -1,44 +1,31 @@
 import random
-
+from random import randint
 
 def random_message():
     message = [random.randint(0,1)for i in range(64)]
     return message
-def burst_error_simulation(message, always_burst_eight):
-    falsified_message = []
-    falsified_message = message
-    burst_length = 0
-    for i in range(64):
-        if burst_length == 0:
-            if random.randint(0,63) == 63:
-                print(f"Burst happened on {i+1} bit")
-                current_sign = falsified_message[i]
-                if current_sign == 1:
-                    falsified_message[i] = 0
-                else:
-                    falsified_message[i] = 1
-                burst_length = 1
-        elif burst_length == 7 or (not always_burst_eight and random.randint(0,8) == 8):
-            current_sign = falsified_message[i]
-            if current_sign == 1:
-                falsified_message[i] = 0
-            else:
-                falsified_message[i] = 1
-            print(f"Burst ended on {i + 1} bit")
-            return falsified_message
-        else:
-            burst_length += 1
-            if random.randint(0,1) == 1:
-                current_sign = falsified_message[i]
-                print(f"Bit {i + 1} was swapped")
-                if current_sign == 1:
-                    falsified_message[i] = 0
-                else:
-                    falsified_message[i] = 1
+def burst_error_simulation(message):
 
-    return falsified_message
+    burst_start = randint(0,63)
+    if 64 - burst_start < 8:
+        burst_length = randint(0, 64 - burst_start)
+    else:
+        burst_length = randint(1, 7)
+    print(message)
+    falsified_bits = []
+    for pos, letter in enumerate(message[burst_start:burst_start + burst_length + 1], burst_start):
+
+        if randint(0,1) == 1 or burst_start + burst_length == pos or burst_start == pos:
+            print(f"Changed {pos}. bit")
+            falsified_bits.append(pos)
+            if letter == "0":
+                message[pos] = "1"
+            else:
+                message[pos] = "0"
+
+    return message, falsified_bits, len(falsified_bits)
 
 
 
 message = random_message()
-falsified_message = burst_error_simulation(message, False)
+falsified_message = burst_error_simulation(message)[0]
